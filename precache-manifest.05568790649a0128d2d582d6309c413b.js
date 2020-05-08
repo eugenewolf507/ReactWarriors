7bc690d92fab69149e3b34ae6adb6f65a0f381f2
Eugene Volkov <eugenewolf507@gmail.com>
2020-05-08 17:31:11 +0300

    Updates

---
+++ precache-manifest.05568790649a0128d2d582d6309c413b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9f1253e2e42361f51152f12bcf60fb",
+    "url": "/MovieApp/index.html"
+  },
+  {
+    "revision": "c775edf95c926fff305d",
+    "url": "/MovieApp/static/css/main.24dd5212.chunk.css"
+  },
+  {
+    "revision": "7e48183db9b4411aa285",
+    "url": "/MovieApp/static/js/2.97ba2bf9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MovieApp/static/js/2.97ba2bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c775edf95c926fff305d",
+    "url": "/MovieApp/static/js/main.3b860b1a.chunk.js"
+  },
+  {
+    "revision": "436392096a04d33fb885",
+    "url": "/MovieApp/static/js/runtime-main.1599e140.js"
+  },
+  {
+    "revision": "c875b997229a3c29478f38a951fa2563",
+    "url": "/MovieApp/static/media/logo.c875b997.svg"
+  }
+]);
