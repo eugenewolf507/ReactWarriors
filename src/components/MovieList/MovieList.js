88882902700c47c672c6b1179d7fd55931299109
Eugene Volkov <eugenewolf507@gmail.com>
2021-02-10 16:40:02 +0200

    fix H2 position in MovieItem and MovieList

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import MovieItem from '../MovieItem/MovieItemContainer';
 import styles from './movieList.module.css';
